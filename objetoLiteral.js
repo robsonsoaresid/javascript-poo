bf1c5de30a7242862ff5e3c15c122a65afca112d
robsonsoaresid <robemsuave@gmail.com>
2026-05-15 20:24:34 -0300

    Adiciona atualizacoes

--- objetoLiteral.js
+++ objetoLiteral.js
@@ -5,8 +5,23 @@ const user = {
   role: "estudante",
   ativo: true,
   exibirInfos: function () {
-    console.log(this.nome, this.role);
+    console.log(this.nome, this.email, this.nascimento, this.role, this.ativo);
   },
 };
 
-user.exibirInfos()
+//user.exibirInfos()
+
+//const exibir = user.exibirInfos
+///exibir()
+
+const exibir = function () {
+  console.log(this.nome, this.email);
+};
+
+const exibirNome = exibir.bind(user)
+exibirNome()
+
+                const numeros = [1, ,2, 3,] // aqui é arrow faction
+                numeros.forEach((numero) => {
+                  console.log(numero)
+                })
