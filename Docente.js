0ae6b19258b16677dad36b8a8da50535b56f9285
robsonsoaresid <robemsuave@gmail.com>
2026-05-20 15:02:38 -0300

    Adiciona docente

---
+++ Docente.js
@@ -0,0 +1,14 @@
+import User from "./User.js";
+
+class Docente extends User {
+  constructor(nome, email, nascimento, role = "docente", ativo = "ativo") {
+    super(nome, email, nascimento, role, ativo);
+  }
+
+  aprovarEstudante(estudante, curso) {
+    return `estudante ${estudante} passou no curso de ${curso}, responsável ${this.nome}`;
+  }
+}
+
+const novaDocente = new Docente("Ana", "a@a.com", "2024-01-01");
+console.log(novaDocente.aprovarEstudante("Juliana", "JavaScript"));
